7241ddcc1ae37a2c3874cd38a6579b1688f2bbbc
Justin Gardner <justin@rhynorater.com>
2025-02-11 06:01:44 -0500

    3.0.26

--- hostinger/vendor/composer/installed.php
+++ hostinger/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'hostinger/hostinger',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '0e6566f04177706bd3b09e4d8f916fb5f265e2ab',
+        'reference' => '3df5478f762a9f7e73e85abd2ec58d49f3f6f7e8',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'hostinger/hostinger' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '0e6566f04177706bd3b09e4d8f916fb5f265e2ab',
+            'reference' => '3df5478f762a9f7e73e85abd2ec58d49f3f6f7e8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
